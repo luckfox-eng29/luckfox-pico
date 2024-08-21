07fd34bbd1b7157d8dfb00cce8d69c79d2172bac
luckfox-eng29 <eng29@luckfox.com>
2024-08-21 15:18:17 +0800

    update:sysdrv-tools

---
+++ sysdrv/tools/board/board/gdb/gdb-10.2/gdbserver/version-generated.cc
@@ -0,0 +1,4 @@
+#include "gdbsupport/version.h"
+const char version[] = "10.2";
+const char host_name[] = "arm-rockchip830-linux-uclibcgnueabihf";
+const char target_name[] = "arm-rockchip830-linux-uclibcgnueabihf";
